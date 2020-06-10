01088bedbb2a4d06d087bb07314a48d4f6fe1c57
Adam Nemeth <nemethadam88@gmail.com>
2020-06-10 22:57:12 +0200

    Updates

---
+++ precache-manifest.31e043583eed0393b4c7867d420d0e35.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "637465190d43e98ce855b8863012d60a",
+    "url": "/cool-clothing/index.html"
+  },
+  {
+    "revision": "ab35c452948ae111ec96",
+    "url": "/cool-clothing/static/css/main.fb5e79b7.chunk.css"
+  },
+  {
+    "revision": "f79bbf5cffc4248a31a1",
+    "url": "/cool-clothing/static/js/2.4646792b.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/cool-clothing/static/js/2.4646792b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab35c452948ae111ec96",
+    "url": "/cool-clothing/static/js/main.951086f2.chunk.js"
+  },
+  {
+    "revision": "5e2af9d2b4210fe2aa91",
+    "url": "/cool-clothing/static/js/runtime-main.cdd42ff3.js"
+  },
+  {
+    "revision": "1219ab733278eb6b0e9591fd8e25b1ce",
+    "url": "/cool-clothing/static/media/Logo.1219ab73.svg"
+  },
+  {
+    "revision": "f215d97107847641116260d3e4c38f71",
+    "url": "/cool-clothing/static/media/close.f215d971.svg"
+  },
+  {
+    "revision": "dda4958f2258d01702162f88396cd6dd",
+    "url": "/cool-clothing/static/media/favorites.dda4958f.svg"
+  },
+  {
+    "revision": "e19509aa015546c2dc9a14b325827efc",
+    "url": "/cool-clothing/static/media/icon-right.e19509aa.svg"
+  },
+  {
+    "revision": "41f887bc622898e1c9d4b7f2679fb8df",
+    "url": "/cool-clothing/static/media/magnify.41f887bc.svg"
+  },
+  {
+    "revision": "0e1bf891d546a789cf5e05872ea01489",
+    "url": "/cool-clothing/static/media/minus-filled.0e1bf891.svg"
+  },
+  {
+    "revision": "1b030740da2d4a221bc69029aaea42a0",
+    "url": "/cool-clothing/static/media/person.1b030740.svg"
+  },
+  {
+    "revision": "5333bb5624c67279c00853369062f546",
+    "url": "/cool-clothing/static/media/plus-filled.5333bb56.svg"
+  },
+  {
+    "revision": "cd7a66c915ad21fe2fa9d4328f361577",
+    "url": "/cool-clothing/static/media/shopbag.cd7a66c9.svg"
+  }
+]);
