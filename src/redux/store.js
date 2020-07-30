5297b97dfad0283458cddb90522e122b6e914b61
Adam Nemeth <16600321+nemad88@users.noreply.github.com>
2020-07-30 18:25:27 +0200

    logger in dev mode

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,8 +6,11 @@ import thunk from 'redux-thunk';
 
 import rootReducer from './root-reducer';
 
-const middlewares = [thunk, logger];
-// const middlewares = [thunk];
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(logger);
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
